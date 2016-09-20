bc765c334d947149389dde0279f014ea67ba70b8
notoaki15 <siderop0ne0@gmail.com>
2016-09-21 00:19:49 +0900

    Add files via upload

---
+++ Qq.java
@@ -0,0 +1,29 @@
+public class Qq{
+	
+	public static void main(String[] args){
+		
+			int x = 1;
+			int y = 1;
+			int p = 1;
+		while(p<82){
+				System.out.print(p);
+				System.out.print("+"+p);
+				System.out.println("="+(p+p));
+				
+				p++;
+				
+			
+				
+				
+				
+				
+			}
+			
+				
+		}
+
+		
+	}
+
+	
+	
